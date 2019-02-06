6406e46a57c7dcb263aab8e02715b5112e8de580
akbacak <33653031+akbacak@users.noreply.github.com>
2019-02-06 09:07:06 +0300

    Add files via upload

---
+++ generateCodes_all.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+from keras.models import Sequential,Input,Model,InputLayer
+from keras.models import model_from_json
+from keras.models import load_model
+import numpy as np    # for mathematical operations
+
+np.set_printoptions(linewidth=1024)
+
+
+json_file = open('models/dmlh2_64_model.json', 'r')
+model = json_file.read()
+json_file.close()
+model = model_from_json(model)
+model.load_weights("models/dmlh2_64_weights.h5")
+model = Model(inputs=model.input, outputs=model.get_layer('dense_2').output)
+
+X = np.load(open('preprocessed_X.npy'))
+X.shape
+
+XX = model.predict(X, batch_size=64, verbose=0, steps=None) #     TRY THIS ALSO XX = model.predict(X, batch_size)
+
+features = XX 
+features = features.astype(int)
+np.savetxt('models/dmlh2_64.txt',features, fmt='%d')
+
+
